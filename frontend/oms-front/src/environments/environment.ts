891821e491816e098366e1cf371c8c9a05fb444e
brianSuarezADL <59981109+brianSuarezADL@users.noreply.github.com>
2020-12-06 09:34:53 -0500

    :rocket: add configuration files

--- frontend/oms-front/src/environments/environment.ts
+++ frontend/oms-front/src/environments/environment.ts
@@ -2,7 +2,7 @@
 //const host = (<any>window).host;
 //Endpoint servicios
 //const hostSeguridad = 'http://localhost:9092';
-const hostApiGateway = 'http://10.39.1.85:9092';
+const HOST_API_GATEWAY = 'http://10.39.1.85:9092';
 
 
 
@@ -13,20 +13,20 @@ const grant_type = 'refresh_token';
 
 export const environment = {
   production: false,
-  login_endpoint: `${hostApiGateway}/uua/oauth/token`,
-  createUser_endpoint: `${hostApiGateway}/registry/users`,
-  searchUser_endpoint: `${hostApiGateway}/security/users`,
+  login_endpoint: `${HOST_API_GATEWAY}/uua/oauth/token`,
+  createUser_endpoint: `${HOST_API_GATEWAY}/registry/users`,
+  searchUser_endpoint: `${HOST_API_GATEWAY}/security/users`,
   clientId_variable: `${clientId}`,
   clientSecret_variable: `${clientSecret}`,
   grant_type_variable: `${grant_type}`,
-  createimage_endpoint: `${hostApiGateway}/images/cmd`,
-  createProduct_endpoint: `${hostApiGateway}/products/cmd`,
-  searchProduct_endpoint: `${hostApiGateway}/products/qrs`,
-  createCampaign_endpoint: `${hostApiGateway}/campaigns/cmd`,
-  searchCampaign_endpoint: `${hostApiGateway}/campaigns/qrs`,
-  searchOrder_endpoint: `${hostApiGateway}/orders/qrs`,
-  cancelOrder_endpoint: `${hostApiGateway}/orders/cmd`,
-  createVendor_endpoint: `${hostApiGateway}/vendors/cmd`,
-  searchVendor_endpoint: `${hostApiGateway}/vendors/qrs`,
-  cancelOrderBPM_endpoint: `${hostApiGateway}/bpm/cancel`
+  createimage_endpoint: `${HOST_API_GATEWAY}/images/cmd`,
+  createProduct_endpoint: `${HOST_API_GATEWAY}/products/cmd`,
+  searchProduct_endpoint: `${HOST_API_GATEWAY}/products/qrs`,
+  createCampaign_endpoint: `${HOST_API_GATEWAY}/campaigns/cmd`,
+  searchCampaign_endpoint: `${HOST_API_GATEWAY}/campaigns/qrs`,
+  searchOrder_endpoint: `${HOST_API_GATEWAY}/orders/qrs`,
+  cancelOrder_endpoint: `${HOST_API_GATEWAY}/orders/cmd`,
+  createVendor_endpoint: `${HOST_API_GATEWAY}/vendors/cmd`,
+  searchVendor_endpoint: `${HOST_API_GATEWAY}/vendors/qrs`,
+  cancelOrderBPM_endpoint: `${HOST_API_GATEWAY}/bpm/cancel`
 };
